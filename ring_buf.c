7c74051391545ed92bea0f4e99717c03a613897c
Chen <garrenchen@163.com>
2026-03-24 18:28:38 +0800

    push_update

--- ring_buf.c
+++ ring_buf.c
@@ -17,6 +17,19 @@ log_ring_t *log_ring_init(uint32_t capacity){
 }
 
 void log_ring_push(log_ring_t* ring,const char* text){
+    if(!ring || !text)  return;
     char* copy = strdup(text);
-    
+    if(!copy)   return;
+
+    if(ring->count == ring->capacity){
+        if(ring->slots[ring->head].content!=NULL){
+        free(ring->slots[ring->head]);
+        ring->slots[ring->head].content=NULL;
+        }
+
+        ring->head=(ring->head+1)%ring->capacity;
+        ring->count--;
+    }
+    int32_t tail = (ring->count + ring->head)%ring->capacity;
+    ring->slots
 }
